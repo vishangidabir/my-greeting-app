ea736370c101332714694cb805931722be75ac86
vishangi <vishangid@gmail.com>
2022-09-03 14:51:04 +0530

    Added : Welcome to my greeting app

---
+++ main/java/com/bridgelabz/mygreetingapp/entity/Greeting.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.mygreetingapp.entity;
+
+public class Greeting {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String message;
+
+    public Greeting(long id, String firstName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.message = message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
